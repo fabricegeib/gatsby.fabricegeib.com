6845677a8067956329e4e110f459f5cea50d98f8
Fabrice Geib <fabricegeib@gmail.com>
2020-01-13 16:17:24 +0100

    add global css

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,8 @@
  */
 
 // You can delete this file if you're not using it
+
+import "./src/styles/global.css"
+// or:
+// require('./src/styles/global.css')
+// When working with files that are only run in a Node.js environment however (like gatsby-node.js), require will need to be used.
