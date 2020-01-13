22dc32f2b6cbfd6b051a402fd66bba9599dacbe0
Fabrice Geib <fabricegeib@gmail.com>
2020-01-13 16:13:08 +0100

    sub-components and props

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,12 +2,14 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
+import Experience from "../components/about/experience"
 import SEO from "../components/seo"
 
 const AboutPage = () => (
   <Layout>
     <SEO title="About" />
     <h1>About</h1>
+    <Experience experienceTitle="mariannecanarelli.com" />
   </Layout>
 )
 
