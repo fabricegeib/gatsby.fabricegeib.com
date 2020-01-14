05ad7c08dbd3753c8a03ef9dab3eed1c418d36c9
Fabrice Geib <fabricegeib@gmail.com>
2020-01-14 01:47:26 +0100

    navigation links between pages

--- src/components/nav.js
+++ src/components/nav.js
@@ -2,18 +2,18 @@ import { Link } from "gatsby"
 // import PropTypes from "prop-types"
 import React from "react"
 
-const Nav = ({ siteTitle }) => (
+const ListLink = props => (
+  <li className="nav__link">
+    <Link to={props.to}>{props.children}</Link>
+  </li>
+)
+
+const Nav = () => (
   <nav className="nav">
     <ul className="nav__links">
-      <li className="nav__link">
-        <Link to="/">home</Link>
-      </li>
-      <li className="nav__link">
-        <Link to="/about/">about</Link>
-      </li>
-      <li className="nav__link">
-        <Link to="/learn/">learn</Link>
-      </li>
+      <ListLink to="/">home></ListLink>
+      <ListLink to="/about/">about</ListLink>
+      <ListLink to="/learn/">learn</ListLink>
     </ul>
   </nav>
 )
