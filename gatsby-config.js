22f4cd8ac1e38ba8d526babb64fb259f1caa0ffb
Fabrice Geib <fabricegeib@gmail.com>
2020-01-15 23:45:34 +0100

    first GraphQL query
    use gatsby-config for siteMetadata and add gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Fabrice Geib`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    siteUrl: `https://fabricegeib.com`,
+    description: `Frontend developer & Integrator from Paris 🇫🇷`,
     author: `Fabrice Geib`,
     twitter: `@fabricegeib`,
     instagram: `@fabricegeib`
@@ -11,8 +12,8 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: `src`,
+        path: `${__dirname}/src/`,
       },
     },
     `gatsby-transformer-sharp`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,28 @@
 import React from "react"
 // import { Link } from "gatsby"
+import { graphql } from "gatsby"
+
 
 import Layout from "../components/layout"
 // import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+export const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+        author
+      }
+    }
+  }
+`
+
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
     <h1>Hello,</h1>
-    <p>I'm Fabrice Geib <span role="img" aria-label="Vulcan Salute">🖖</span></p>
+    <p>I'm {data.site.siteMetadata.author} <span role="img" aria-label="Vulcan Salute">🖖</span></p>
     <p>Frontend developer & Integrator</p>
   </Layout>
 )
